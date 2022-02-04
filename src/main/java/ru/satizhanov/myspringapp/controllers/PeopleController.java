2dbc992ce294edcf44e273072de4a14658da22df
ipskiff <ipskiff007@gmail.com>
2022-02-05 01:43:05 +0200

    2. CRUD, REST, Паттерн DAO. +create(). @ModelAttribute

--- src/main/java/ru/satizhanov/myspringapp/controllers/PeopleController.java
+++ src/main/java/ru/satizhanov/myspringapp/controllers/PeopleController.java
@@ -3,10 +3,9 @@ package ru.satizhanov.myspringapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.satizhanov.myspringapp.dao.PersonDAO;
+import ru.satizhanov.myspringapp.models.Person;
 
 @Controller
 @RequestMapping("/people")
@@ -29,6 +28,18 @@ public class PeopleController {
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("person", personDAO.show(id));
         return "people/show";
+    }
+    
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+//        model.addAttribute("person", new Person());
+        return "people/new";
+    }
+
+    @PostMapping()
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
     }
 
 }
